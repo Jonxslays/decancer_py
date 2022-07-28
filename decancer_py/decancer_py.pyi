934bb7a4c575ad9adb8c46f9b8624939f0e44013
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-07-27 18:42:17 -0700

    Update docstring [skip ci]

--- decancer_py/decancer_py.pyi
+++ decancer_py/decancer_py.pyi
@@ -9,11 +9,11 @@ def contains(string: str, text: str, parse: bool = True) -> bool:
     Args:
         string (`str`): The string to search through
         text (`str`): The text to check for
-        parse: (`bool`): If True, parse the string first.
-            Defaults to True.
+        parse (`bool`): If True, parse the string first
+            Defaults to True
 
     Returns:
-        `bool` - `True` if the text is contained in the string
+        `bool` - True if the text is contained in the string
     """
 def parse(text: str) -> str:
     """Parses a jank string into a less toxic lowercase string.
