ee41c4c93afb614ceb3c2efb3c9bccd54c647556
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-07-27 18:02:54 -0700

    Add rust tests

--- src/lib.rs
+++ src/lib.rs
@@ -29,3 +29,36 @@ fn decancer_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(contains, m)?)?;
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::contains;
+    use crate::parse;
+
+    #[test]
+    fn test_contains_when_parse_is_none() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert!(contains(yeet.clone(), "ye".to_string(), None));
+        assert!(!contains(yeet, "no".to_string(), None));
+    }
+
+    #[test]
+    fn test_contains_when_parse_is_true() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert!(contains(yeet.clone(), "ye".to_string(), Some(true)));
+        assert!(!contains(yeet, "no".to_string(), None));
+    }
+
+    #[test]
+    fn test_contains_when_parse_is_false() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert!(contains("yeet".to_string(), "ye".to_string(), Some(false)));
+        assert!(!contains(yeet.clone(), "ye".to_string(), Some(false)));
+    }
+
+    #[test]
+    fn test_parse() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert_eq!(parse(yeet), String::from("yeet"));
+    }
+}
