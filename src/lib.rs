b6b2bbe019a99ce48df2a137584865e3ceba5607
Leo Developer <leodeveloper@protonmail.com>
2022-12-25 18:40:16 +0100

    update to new api

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,32 @@
-use decancer::Decancer;
+extern crate decancer;
 use pyo3::prelude::*;
-
-const BRIDGE: Decancer = Decancer::new();
 
 /// Parses a jank string into a less toxic lowercase string.
 #[pyfunction]
 #[pyo3(text_signature = "(text: str) -> str")]
 pub fn parse(text: String) -> String {
-    BRIDGE.cure(&text)
+    decancer::cure(&text).into_str()
 }
 
 /// Check whether some string contains some text.
 #[pyfunction]
-#[pyo3(text_signature = "(string: str, text: str, parse: bool = True) -> bool")]
-pub fn contains(mut string: String, text: String, parse: Option<bool>) -> bool {
-    if parse.unwrap_or(true) {
-        string = BRIDGE.cure(&string)
-    }
+#[pyo3(text_signature = "(string: str, text: str) -> bool")]
+pub fn contains(string: String, text: String) -> bool {
+    decancer::cure(&string).contains(&text)
+}
 
-    BRIDGE.contains(&string, &text)
+/// Check whether a string starts with some text.
+#[pyfunction]
+#[pyo3(text_signature = "(string: str, text: str) -> bool")]
+pub fn starts_with(string: String, text: String) -> bool {
+    decancer::cure(&string).starts_with(&text)
+}
+
+/// Check whether a string ends with some text.
+#[pyfunction]
+#[pyo3(text_signature = "(string: str, text: str) -> bool")]
+pub fn ends_with(string: String, text: String) -> bool {
+    decancer::cure(&string).ends_with(&text)
 }
 
 /// The module we export to python
@@ -27,38 +35,39 @@ fn decancer_py(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add("__version__", std::env!("CARGO_PKG_VERSION"))?;
     m.add_function(wrap_pyfunction!(parse, m)?)?;
     m.add_function(wrap_pyfunction!(contains, m)?)?;
+    m.add_function(wrap_pyfunction!(starts_with, m)?)?;
+    m.add_function(wrap_pyfunction!(ends_with, m)?)?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::contains;
-    use crate::parse;
+    use crate::{contains, starts_with, ends_with, parse};
 
     #[test]
-    fn test_contains_when_parse_is_none() {
+    fn test_contains() {
         let yeet = String::from("𝔂ＥＥ𝓣");
-        assert!(contains(yeet.clone(), "ye".to_string(), None));
-        assert!(!contains(yeet, "no".to_string(), None));
-    }
-
-    #[test]
-    fn test_contains_when_parse_is_true() {
-        let yeet = String::from("𝔂ＥＥ𝓣");
-        assert!(contains(yeet.clone(), "ye".to_string(), Some(true)));
-        assert!(!contains(yeet, "no".to_string(), None));
-    }
-
-    #[test]
-    fn test_contains_when_parse_is_false() {
-        let yeet = String::from("𝔂ＥＥ𝓣");
-        assert!(contains("yeet".to_string(), "ye".to_string(), Some(false)));
-        assert!(!contains(yeet.clone(), "ye".to_string(), Some(false)));
+        assert!(contains(yeet.clone(), "ye".to_string()));
+        assert!(!contains(yeet, "no".to_string()));
     }
 
     #[test]
     fn test_parse() {
         let yeet = String::from("𝔂ＥＥ𝓣");
         assert_eq!(parse(yeet), String::from("yeet"));
+    }
+
+    #[test]
+    fn test_starts_with() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert!(starts_with(yeet.clone(), "ye".to_string()));
+        assert!(!starts_with(yeet.clone(), "et".to_string()));
+    }
+
+    #[test]
+    fn test_ends_with() {
+        let yeet = String::from("𝔂ＥＥ𝓣");
+        assert!(!ends_with(yeet.clone(), "ye".to_string()));
+        assert!(ends_with(yeet.clone(), "et".to_string()));
     }
 }
