361758c3992411f58cacf81c6519eff174ab0b1b
Jonxslays <51417989+Jonxslays@users.noreply.github.com>
2022-12-26 18:04:12 -0700

    Update tests

--- tests/test_decancer.py
+++ tests/test_decancer.py
@@ -2,18 +2,27 @@ from decancer_py import parse
 
 YEET = parse("𝔂ＥＥ𝓣")
 
+
 def test_contains() -> None:
     assert YEET.contains("ee")
     assert not YEET.contains("no")
+
 
 def test_starts_with() -> None:
     assert YEET.starts_with("ye")
     assert not YEET.starts_with("et")
 
+
 def test_ends_with() -> None:
     assert YEET.ends_with("et")
     assert not YEET.ends_with("ye")
 
+
 def test_equals() -> None:
-    assert YEET == "YEET"
-    assert not YEET == "NO"
+    assert YEET == "yeet"
+    assert YEET != "no"
+
+
+def test_dunder_contains() -> None:
+    assert "ee" in YEET
+    assert "no" not in YEET
